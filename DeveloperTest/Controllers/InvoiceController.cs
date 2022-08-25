cde7f8e0471ac507255a2252e220fda84577ad0f
Pavel Pilař <pavel.pilar1@gmail.com>
2022-08-25 19:25:28 +0200

    Create placeholder endpoints

--- DeveloperTest/Controllers/InvoiceController.cs
+++ DeveloperTest/Controllers/InvoiceController.cs
@@ -3,8 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 namespace DeveloperTest.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/invoices")]
 public class InvoiceController : ControllerBase
 {
-    
+    [HttpGet("unpaid")]
+    public ActionResult GetUnpaidInvoices()
+    {
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public ActionResult EditInvoice()
+    {
+        return NoContent();
+    }
+
+    [HttpPost("{id}")]
+    public ActionResult PayInvoice()
+    {
+        return NoContent();
+    }
 }
