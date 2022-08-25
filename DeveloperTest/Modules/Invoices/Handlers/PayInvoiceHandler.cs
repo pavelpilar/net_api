b13c31b61c13e334dc5efbf1071301182bcbb77f
Pavel Pilař <pavel.pilar1@gmail.com>
2022-08-25 20:31:02 +0200

    Implement PayInvoice handler

--- DeveloperTest/Modules/Invoices/Handlers/PayInvoiceHandler.cs
+++ DeveloperTest/Modules/Invoices/Handlers/PayInvoiceHandler.cs
@@ -4,8 +4,30 @@ namespace DeveloperTest.Modules.Invoices.Handlers
 {
     public class PayInvoiceHandler : IRequestHandler<PayInvoiceRequest, PayInvoiceResponse>
     {
+        private readonly InvoiceContext ctx;
+
+        public PayInvoiceHandler(InvoiceContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
         public async Task<PayInvoiceResponse> Handle(PayInvoiceRequest request, CancellationToken cancellationToken)
         {
+            var invoice = ctx.Invoices.SingleOrDefault(x => x.Id == request.Id);
+
+            if(invoice == null)
+            {
+                throw new Exception();
+            }
+
+            if(invoice.Paid)
+            {
+                throw new Exception();
+            }
+
+            invoice.Paid = true;
+            ctx.SaveChanges();
+
             return new PayInvoiceResponse();
         }
     }
